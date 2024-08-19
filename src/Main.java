42b9f7a1f8418d35f429358718f405057b4c77a7
elena kolesnikova <elena_kolesnikov@list.ru>
2024-08-19 09:08:10 +0200

    =выполненное задание

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int startBalance = 400;//Баланс начальный
+        int appendSum = 1100;//Сумма пополнения
+        int bonusSum = 0;   //Кол-во начисляемых бонус
+        if(appendSum > 1000){//Если сумма пополнения больше 1000
+            bonusSum = appendSum / 100;//Кол-во начисляемых бонусов = сумма пополнения делить на 100 рублей
+        }
+        int resultBalance = startBalance + appendSum + bonusSum;//результат после пополнения
+        System.out.println("Итоговый баланс = " + resultBalance + " Кол-во бонусов = " + bonusSum);
     }
 }
