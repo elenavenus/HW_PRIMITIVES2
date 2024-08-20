92d0582d197126b167a3da67a06f9d77ae1316fb
elena kolesnikova <elena_kolesnikov@list.ru>
2024-08-20 08:11:02 +0200

    =Добавили автоформатирование

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int startBalance = 400;//Баланс начальный
         int appendSum = 1100;//Сумма пополнения
         int bonusSum = 0;   //Кол-во начисляемых бонус
-        if(appendSum > 1000){//Если сумма пополнения больше 1000
+        if (appendSum > 1000) {//Если сумма пополнения больше 1000
             bonusSum = appendSum / 100;//Кол-во начисляемых бонусов = сумма пополнения делить на 100 рублей
         }
         int resultBalance = startBalance + appendSum + bonusSum;//результат после пополнения
